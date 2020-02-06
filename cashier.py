8c2c3db62890281f27740725a2d237483a1566d4
Ahmad-Saadeh <Ahmad-Saadeh@users.noreply.github.com>
2020-02-06 15:44:03 +0200

    cashier

--- cashier.py
+++ cashier.py
@@ -1,5 +1,24 @@
 def main():
 	# write code here
+	total=0
+	counter = "done"
+	item =""
+	myList = []
+	while item!=counter:
+		item = input('Item (enter "done" when finished): ')
+		if item==counter:
+			break
+		price = input('Price: ')
+		quantity = input('Quantity: ')
+		a = {'name':item ,'price':price ,'quantity':quantity}
+		myList.append(a)
+	#print(myList)
+	print("\n------------------- \n receipt \n------------------- \n ")
+	for key in myList:
+		multiply = float(key['price']) * float(key['quantity'])
+		print(key['quantity'],key['name'],multiply,"KD")
+		total += multiply
+	print("------------------\n Total price: {}KD".format(total))
 
 if __name__ == '__main__':
 	main()
